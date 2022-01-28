1c3656a0ce9c4539c3db91850a48e0169050cd5c
Régis BRUNET BLEC <regis.blec@gmail.com>
2022-01-28 08:00:01 +0100

    intégration de thibaud

---
+++ src/assets/datas/thibaud.js
@@ -0,0 +1,19 @@
+const Thibaud = {
+  firstname: "Thibaud",
+  lastname: "Fournès",
+  tel: "+33638674965",
+  email: "thibaud.fournes@gmailcom",
+  github: "https://github.com/ThibaudFournes77",
+  linkedin: "https://www.linkedin.com/in/thibaud-fournes-ingenieur-developpeur-full-stack",
+  smartr: "",
+  indeed: "https://my.indeed.com/p/thibaudf-5ykh0o6",
+  role: [
+    "Dev back",
+    "Dev front",
+  ],
+  teckBack: ["Nodejs"],
+  teckFront: ["React", "Redux", "SCSS"],
+  realisation: ["Correction de bugs", "Réorganisation du code"],
+  bdd: ["PostgreSQL"],
+};
+export default Thibaud;
--- src/components/Developpeur/index.js
+++ src/components/Developpeur/index.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import imgRegis from "../../assets/Régis.jpg";
 import imgYan from "../../assets/Yan.jpg";
+import imgThibaud from "../../assets/Thibaud.jpg";
 import Card from "./Card";
 import Regis from "../../assets/datas/regis";
 import Yan from "../../assets/datas/yan";
+import Thibaud from "../../assets/datas/thibaud";
+
 import "./style.scss";
 
 const Developpeur = () => {
@@ -12,11 +15,12 @@ const Developpeur = () => {
     <div className="paragraphe">
       <div className="paragraphe__border">
         <h2 className="paragraphe__title">
-          Voici les développeurs ayant participé au projets
+          Voici les développeurs ayant participé au projet
         </h2>
         <div className="paragraphe__card">
           <Card perso={Regis} img={imgRegis} />
           <Card perso={Yan} img={imgYan} />
+          <Card perso={Thibaud} img={imgThibaud} />
         </div>
       </div>
     </div>
